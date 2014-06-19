46aa1011baa60cbcd0672d776bc89a42f50823e9
aarich <arich@hmc.edu>
2014-06-19 13:31:11 +1000

    Match one image to many, saves matches as jpgs

---
+++ build/CMakeFiles/matching_to_many_images.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/robotics/Desktop/OpenCV_Set/matching_to_many_images.cpp" "/home/robotics/Desktop/OpenCV_Set/build/CMakeFiles/matching_to_many_images.dir/matching_to_many_images.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "ROS_PACKAGE_NAME=\"connect_kinect\""
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
